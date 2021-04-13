3521c6a6678ec975ec81cb3bfa189b2b10874f07
Letretch <tahamohammedi@yahoo.com>
2021-04-13 12:26:58 +0000

    adding json support

--- execute.py
+++ execute.py
@@ -1,10 +1,22 @@
 import mechanicalsoup
+import json
+import time
 
-def main(url):
-	browser = mechanicalsoup.Browser()
-	page = browser.get(url)
-	print(page)
+def main(url, timestamp):
+	starttime = time.time()
+	with open('htm.html') as html:
+		page = html.read()
+	while True:
+		with open('htm.html') as html:
+			new_page = html.read()
+		trackchange(page, new_page)
+		time.sleep(timestamp - ((time.time() - starttime) % timestamp))
 
+def trackchange(page, new_page):
+	with open('htm.json') as js:
+		page_json = json.loads(js.read())
+
+	print(page_json["body"])
 
 if __name__ == '__main__':
 	main()
--- webcrawler.py
+++ webcrawler.py
@@ -6,16 +6,20 @@ from requests.exceptions import ConnectionError
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--url", "-u", help="provide the url of the web page")
-
+parser.add_argument("--timestamp",  "-t", help="specifies the delay between each request (a request every 60 seconds is the default value)", type=int, default=60)
 
 
 def main():
 	args = parser.parse_args()
 	url = args.url
-	execute.main(url)
-
-
-
+	try:
+		execute.main(url, args.timestamp)
+	except Exception as exception:
+		if "Name or service not known" in str(exception):
+			print("ConnectionError: URL Could Not Be Found.")
+		if "Temporary failure in name resolution" in str(exception):
+			return  print("ConnectionError: There's No Internet Connection")
+		return print(exception)
 
 
 
